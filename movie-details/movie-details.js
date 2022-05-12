0d767b17c7ed368def258ec2b4e7dfb365154d14
Brien Thomas <thomasbrien94@gmail.com>
2022-05-12 13:00:21 -0700

    dropdown menu sort of works, need to figure out how to make it display the list when hovering over the list items

--- movie-details/movie-details.js
+++ movie-details/movie-details.js
@@ -4,7 +4,7 @@ const movieName = document.getElementById('movie-name');
 const movieImage = document.getElementById('movie-image');
 const movieGenre = document.getElementById('movie-genre');
 const movieStar = document.getElementById('movie-star');
-
+const movieTitle = document.getElementById('title');
 
 async function loadMovieDetails() {
     const parameters = new URLSearchParams(window.location.search);
@@ -14,6 +14,7 @@ async function loadMovieDetails() {
     movieImage.src = `.${movie.img}`;
     movieGenre.textContent = `Genre: ${movie.genre}`;
     movieStar.textContent = `Starring: ${movie.actor}`;
+    movieTitle.textContent = `${movie.title}`;
 }
 
 loadMovieDetails();
