0767a6091989010f0b10fb6736d7da26daefc785
Brien Thomas <thomasbrien94@gmail.com>
2022-05-12 17:10:59 -0700

    removed unused code

--- movie-details/movie-details.js
+++ movie-details/movie-details.js
@@ -18,11 +18,3 @@ async function loadMovieDetails() {
 }
 
 loadMovieDetails();
-
-function classToggle() {
-    const navs = document.querySelectorAll('.navbar-items');
-
-    navs.forEach(nav => nav.classList.toggle('navbar-toggleShow'));
-}
-
-document.querySelector('.navbar-toggle').addEventListener('click', classToggle);
